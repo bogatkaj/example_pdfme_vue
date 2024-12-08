4dda70ec553923a02fb1ee6ec0f39e3b6a6c9198
Jan-Niklas Bogatka <102136993+bogatkaj@users.noreply.github.com>
2024-12-08 09:52:07 +0100

    Create print.vue

---
+++ src/layouts/print.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-page-container>
+      <router-view :style="{ 'min-height': 100 + 'px' }"/>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  setup() {
+    
+  },
+};
+</script>
